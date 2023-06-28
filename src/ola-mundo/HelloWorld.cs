e6b5b500cbf04ddd3b7144f6b41bac1e2a960baa
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-06-28 17:41:52 -0300

    Initial commit

---
+++ src/ola-mundo/HelloWorld.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ola_mundo;
+
+public class HelloWorld
+{
+    // 1 - Retorne a string 'Olá, Mundo!'
+    public static string Greet()
+    {
+        throw new NotImplementedException();
+    }
+    // 2 - Retorne a string 'Seja bem-vindo, Tryber!
+    public static string GreetTryber()
+    {
+        throw new NotImplementedException();
+    }
+}
